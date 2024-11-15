c3e4686d97a29c6cb2f98eb263594132808b4966
fac3m4n <akerimberdi@gmail.com>
2024-11-15 11:22:41 +0200

    typo

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   printWidth: 120,
   tabWidth: 2,
   trailingComma: "all",
-  importOrder: ["^react$", "^next/(.*)$", "<THIRD_PARTY_MODULES>", "^@heroicons/(.*)$", "^~~/(.*)$"],
+  importOrder: ["^react$", "^next/(.*)$", "<THIRD_PARTY_MODULES>", "^@heroicons/(.*)$", "^@/(.*)$"],
   importOrderSortSpecifiers: true,
   plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
 };
